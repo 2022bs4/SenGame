cbf0917f24e3bad7581f889a4441d2e534292058
Rain <bboyskyovtc@gmail.com>
2022-06-17 23:36:13 +0800

    feat:聊天介面 *

---
+++ SenGame/Controllers/FriendController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SenGame.Controllers
+{
+    public class FriendController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
