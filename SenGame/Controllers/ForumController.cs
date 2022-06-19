17b44436e565f124dab2441fc19e481a603d49b9
Civic <ert3455668@gmail.com>
2022-06-19 15:04:19 +0800

    feat:新增Forum控制器還有兩view * 新增forum版面的Layoutforum

---
+++ SenGame/Controllers/ForumController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SenGame.Controllers
+{
+    public class ForumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ComomunityHome()
+        {
+            return View();
+        }
+        public IActionResult ComomunityDynamicwall()
+        {
+            return View();
+        }
+    }
+}
