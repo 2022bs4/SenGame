29fa26bc0dddf12e69ddf20d80b25b696c393e6f
Gigi <gitt.gugo@gmail.com>
2022-06-23 20:27:44 +0800

    feat: 新增願望清單假資料 邀請清單假資料 fix: 修補好友列表bug

--- SenGame/FakeDate/FakeUser.cs
+++ SenGame/FakeDate/FakeUser.cs
@@ -25,9 +25,25 @@ namespace SenGame.Data
             //    new User { UserId = 2, UsernickName = "2哈哈是我啦", UserAbout = "午安你好", UserPicture = "網址", UserCountryId = 3 },
             //    new User { UserId = 3, UsernickName = "3哈哈是我啦", UserAbout = "晚安你好", UserPicture = "網址", UserCountryId = 2 }
             //};
-
-            
         }
+
+        //願望清單
+        //public DbSet<Wish> Wish { get; set; }
+        //List<Wish> wishes = new List<Wish>
+        //{
+        //    new Wish { WishId = 0, UserId = 0, GameId = 0, AddDate = DateTime.Now },
+        //    new Wish { WishId = 1, UserId = 1, GameId = 1, AddDate = DateTime.Now },
+        //    new Wish { WishId = 2, UserId = 2, GameId = 2, AddDate = DateTime.Now }
+        //};
+
+        //邀請
+        //public DbSet<Invite> Invit { get; set; }
+        //List<Invite> invites = new List<Invite>
+        //{
+        //    new Invite { InviteId = 0, UserId = 0, SenderId = 0 , Message = "此人申請為您好友"},
+        //    new Invite { InviteId = 1, UserId = 1, SenderId = 1 , Message = "收到邀請通知"},
+        //    new Invite { InviteId = 2, UserId = 2, SenderId = 2 , Message = "邀請訊息!!!"},
+        //};
 
 
     }
