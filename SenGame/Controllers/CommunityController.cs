1eb88e1d1fdf2c024b390629f0725d3f5efc213d
Civic <ert3455668@gmail.com>
2022-06-27 13:39:07 +0800

    fix:社群首頁版面修改

--- SenGame/Controllers/CommunityController.cs
+++ SenGame/Controllers/CommunityController.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SqlModels.Models;
+using SqlModels.ViewModels;
+using System.Collections.Generic;
 
 namespace SenGame.Controllers
 {
     public class CommunityController : Controller
     {
+       
+       
         public IActionResult Index()
         {
+
             return View();
         }
         //顯示討論區
---
+++ SenGame/Migrations/20220625152640_zxc.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SenGame.Migrations
+{
+    public partial class zxc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
