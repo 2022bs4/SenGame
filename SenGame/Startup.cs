2c96ebc1b30726ba7081655c9db1ceb841d8f7f8
Rain <bboyskyovtc@gmail.com>
2022-07-01 18:26:05 +0800

    fix:DI Container註冊google、FB

--- SenGame/Startup.cs
+++ SenGame/Startup.cs
@@ -41,27 +41,22 @@ namespace SenGame
             services.AddDefaultIdentity<UserModel>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<SenGameContext>();
             services.AddControllersWithViews();
-            services.AddTransient<IRepository, GenericRepository>();
+            //services.AddTransient<IRepository, GenericRepository>();
       
             services.AddSignalR();
 
             //µù¥UGoogle¡BFB
-            services.AddAuthentication()
-                .AddGoogle(options =>
-                {
-                    IConfigurationSection googleAuthNSection =
-                        Configuration.GetSection("Authentication:Google");
-
-                    options.ClientId = googleAuthNSection["ClientId"];
-                    options.ClientSecret = googleAuthNSection["ClientSecret"];
-                });
+            services.AddAuthentication().AddGoogle(googleOptions =>
+            {
+                googleOptions.ClientId = "319105508231-b14a9vlbq3sobjfruc8sd8kp9iplgrkf.apps.googleusercontent.com";
+                googleOptions.ClientSecret = "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";
+            });
             services.AddAuthentication().AddFacebook(options =>
             {
-                options.AppId = Configuration["Authentication:Facebook:AppId"];
-                options.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+                options.AppId = "363016615774590";
+                options.AppSecret = "f3ccc3f70ef3b114a2d0fce1562be7c1";
                 options.AccessDeniedPath = "/AccessDeniedPathInfo";
             });
-
 
         }
 
--- SqlModels/Repository/GenericRepository.cs
+++ SqlModels/Repository/GenericRepository.cs
@@ -1,72 +1,72 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Linq.Expressions;
-using SqlModels.Repository.Interface;
-using SqlModels.Data;
-namespace SqlModels.Repository
-{
-    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
-    {
-        private SenGameContext _context;
-        public GenericRepository(SenGameContext context)
-        {
-            this._context = context;
-        }
-        public void Dispose()
-        {
-            this.Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-        //連接操作後進行手動的回收
-        public void Dispose(bool disposing)
-        {
-            //如果能被回收釋放
-            if (disposing)
-            {
-                //如果有要回收釋放的資源
-                if (_context != null)
-                {
-                    //調用方法回收
-                    _context.Dispose();
-                    _context = null;
-                }
-            }
-        }
-        public int SaveChanges()
-        {
-            int result = _context.SaveChanges();
-            return result;
-        }
+﻿//using System;
+//using Microsoft.EntityFrameworkCore;
+//using System.Linq;
+//using System.Linq.Expressions;
+//using SqlModels.Repository.Interface;
+//using SqlModels.Data;
+//namespace SqlModels.Repository
+//{
+//    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+//    {
+//        private SenGameContext _context;
+//        public GenericRepository(SenGameContext context)
+//        {
+//            this._context = context;
+//        }
+//        public void Dispose()
+//        {
+//            this.Dispose(true);
+//            GC.SuppressFinalize(this);
+//        }
+//        //連接操作後進行手動的回收
+//        public void Dispose(bool disposing)
+//        {
+//            //如果能被回收釋放
+//            if (disposing)
+//            {
+//                //如果有要回收釋放的資源
+//                if (_context != null)
+//                {
+//                    //調用方法回收
+//                    _context.Dispose();
+//                    _context = null;
+//                }
+//            }
+//        }
+//        public int SaveChanges()
+//        {
+//            int result = _context.SaveChanges();
+//            return result;
+//        }
 
-        public void Create(TEntity TEntity)
-        {
-            _context.Entry(TEntity).State = EntityState.Added;
-        }
+//        public void Create(TEntity TEntity)
+//        {
+//            _context.Entry(TEntity).State = EntityState.Added;
+//        }
 
-        public void Update(TEntity TEntity)
-        {
-            _context.Entry(TEntity).State = EntityState.Modified;
-        }
+//        public void Update(TEntity TEntity)
+//        {
+//            _context.Entry(TEntity).State = EntityState.Modified;
+//        }
 
-        public void Delete(TEntity TEntity)
-        {
-            _context.Entry(TEntity).State = EntityState.Deleted;
-        }
+//        public void Delete(TEntity TEntity)
+//        {
+//            _context.Entry(TEntity).State = EntityState.Deleted;
+//        }
 
-        public IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate)
-        {
-            return _context.Set<TEntity>().Where(predicate); 
-        }
+//        public IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate)
+//        {
+//            return _context.Set<TEntity>().Where(predicate); 
+//        }
 
-        public IQueryable<TEntity> GetAll()
-        {
-            return _context.Set<TEntity>();
-        }
+//        public IQueryable<TEntity> GetAll()
+//        {
+//            return _context.Set<TEntity>();
+//        }
 
-        public TEntity GetById(int _Id)
-        {
-            return _context.Set<TEntity>().Find(_Id);
-        }
-    }
-}
+//        public TEntity GetById(int _Id)
+//        {
+//            return _context.Set<TEntity>().Find(_Id);
+//        }
+//    }
+//}
