2c526584a61185a68acda4dcc643bd15158efd93
2022bs4 <106955783+2022bs4@users.noreply.github.com>
2022-07-02 22:11:22 +0800

    test:資料庫測試前資料備存 *

--- SqlModels/ViewModels/ProductDetailsViewModel.cs
+++ SqlModels/ViewModels/ProductDetailsViewModel.cs
@@ -9,30 +9,18 @@ namespace SqlModels.ViewModels
         public string GameName { get; set; }
         public decimal GamePrice { get; set; }
         public string GameIntroduction { get; set; }
-        public string GameDetailsText { get; set; }
         public DateTime ReleaseTime { get; set; }
         public string Developer { get; set; }
         public string Marker { get; set; }
 
-        //系統規格
-        public int? Configure { get; set; }
-        public int SystemType { get; set; }
-        public string Hddspace { get; set; }
-        public string System { get; set; }
-        public string SystemRam { get; set; }
-        public string SystemCpu { get; set; }
-        public string SystemGpu { get; set; }
-
-
-        //媒體
         //簡介圖
-        public string MediaUrl { get; set; }
+        public string ProductMainPicture { get; set; }
 
-        //詳細圖
-        public string HearderDetails { get; set; }
-        public string EndDetails { get; set; }
+        //標籤
+        public string TypleName { get; set; }
 
-        //影片
-        public string MediaVideo { get; set; }
+        //折扣
+        public double DisscountTake { get; set; }
+        
     }
 }
