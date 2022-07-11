b0185e4b034977449a63e88da81924c077bedf72
Civic <ert3455668@gmail.com>
2022-07-11 17:27:16 +0800

    feat:新增UserController&Service&IService&DTO

---
+++ Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using Services.Interface;
+using SqlModels.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class UserService : BaseService, IUserService
+    {
+        public UserService(IRepository repository) : base(repository)
+        {
+
+        }
+    }
+}
